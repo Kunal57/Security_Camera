e94980ff826e143d5395f62d9887aa5472876c92
Kunal Patel <kunalpatelx92@gmail.com>
2017-10-13 17:24:30 -0500

    Set up notifications with Twilio

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
 import cv2
 import numpy as np
+from twilio.rest import TwilioRestClient
 from skimage.measure import compare_ssim
+
+ACCOUNT_SID = 'ACCOUNT_SID'
+AUTH_TOKEN = 'AUTH_TOKEN'
+TWILIO_PHONE = 'TWILIO_PHONE'
+RECEIVER_PHONE = 'RECEIVER_PHONE'
 
 def ssim(A,B):
   return compare_ssim(A,B, data_range=A.max()-A.min())
 
 def mse(A,B):
   return ((A-B)**2).mean()
+
+client = TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN)
 
 # Video Input
 cap = cv2.VideoCapture('Shooting_Form.mp4')
@@ -18,6 +26,7 @@ curr_frame = None
 prev_frame = None
 first_frame = True
 frame_counter = 0
+first_msg = True
 
 while True:
   if frame_counter == 0:
@@ -33,7 +42,10 @@ while True:
 
   if frame_counter == 9:
     # print(mse(curr_frame, prev_frame))
-    print(ssim(curr_frame, prev_frame))
+    ssim_index = ssim(curr_frame, prev_frame)
+    if ssim_index < 0.8 and first_msg:
+      client.messages.create(to=RECEIVER_PHONE, from_=TWILIO_PHONE, body="Intruder Alert!")
+      first_msg = False
     frame_counter = 0
 
   cv2.imshow('app', curr_frame)
