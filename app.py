c9eb049809b6f8a8838fa9efa6cd0d7abbee9323
Kunal Patel <kunalpatelx92@gmail.com>
2017-10-13 16:14:19 -0500

    Detect Changes between Frames using MSE & SSIM

--- app.py
+++ app.py
@@ -1,5 +1,12 @@
 import cv2
 import numpy as np
+from skimage.measure import compare_ssim
+
+def ssim(A,B):
+  return compare_ssim(A,B, data_range=A.max()-A.min())
+
+def mse(A,B):
+  return ((A-B)**2).mean()
 
 # Video Input
 cap = cv2.VideoCapture('Shooting_Form.mp4')
@@ -7,11 +14,25 @@ cap = cv2.VideoCapture('Shooting_Form.mp4')
 # Camera Input
 # cap = cv2.VideoCapture(0)
 
+curr_frame = None
+prev_frame = None
+first_frame = True
+
 while True:
-  _, frame = cap.read()
-  if frame is None:
+  prev_frame = curr_frame
+  _, curr_frame = cap.read()
+  if curr_frame is None:
     break
-  cv2.imshow('app', frame)
+
+  curr_frame = cv2.cvtColor(curr_frame, cv2.COLOR_BGR2GRAY)
+  if first_frame:
+    prev_frame = curr_frame
+    first_frame = False
+
+  # print(mse(curr_frame, prev_frame))
+  # print(ssim(curr_frame, prev_frame))
+
+  cv2.imshow('app', curr_frame)
 
   if cv2.waitKey(1) & 0xFF == ord('q'):
     break
